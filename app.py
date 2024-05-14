d9a2c7531833546166848418ab0aaa3d5c335049
oldkingOK <oldkingok@qq.com>
2024-05-14 14:59:27 +0800

    perf(app.py): 优化数据库操作

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, request, render_template
 import sqlite3
+import atexit
 
 app = Flask(__name__)
+
+# 线程开始的时候
+@app.before_request
+def before_first_request():
+    global conn, cursor
+
+    conn = sqlite3.connect('ebook2024-05-13.db', check_same_thread=False)
+    atexit.register(lambda: conn.close())
+    cursor = conn.cursor()
 
 INDEX = "./index.html"
 @app.route('/')
 def sum_numbers():
+    global conn, cursor
 
     name = request.args.get('name')
     if name is None:
         return render_template(INDEX)
 
-    conn = sqlite3.connect('ebook2024-05-13.db')
-    cursor = conn.cursor()
-
     # Execute a SELECT query to retrieve the record with the given name
     cursor.execute("SELECT Path, Text FROM ebook WHERE name LIKE ?", (f"%{name}%",))
     record = cursor.fetchall()
-    conn.close()
 
     r = ""
     if record:
